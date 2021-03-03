8965e87f16cee262ba54e5d60678fe18bce0ea2b
fgfMicrosoftTwT <76402835+fgfMicrosoftTwT@users.noreply.github.com>
2021-03-03 15:00:14 +0100

    Add files via upload

---
+++ HUH/HUH/Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ServiceController1 = New System.ServiceProcess.ServiceController
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(366, 261)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.ImeMode = System.Windows.Forms.ImeMode.[On]
+        Me.Name = "Form1"
+        Me.Text = "Process X_X"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ServiceController1 As System.ServiceProcess.ServiceController
+
+End Class
---
+++ HUH/HUH/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Start()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+            DialogResult dialog = MessageBox.Show("Tem program może wyżądzić szkody w systemie Windows, jezeli nie chesz uruchaniać aplikacji kliknij na przycisk No, tak, yes", "Uwaga!", MessageBoxButtons.YesNo));
+            If(DialogResult = Windows.Forms.DialogResult.Yes);
+            {
+            Dim AcessPros = Microsoft.Win32.Registry.CurrentConfig.OpenSubKey("SOFTWARE\\MICROSOFT\\WINDOWS\\CurrentVersion\\Diagnostics\\DiagTrack")
+            }
+
+        If(AcessPros == False)
+        {
+            AcessPros.SetValue(0);
+        }
+
+                ServiceController1.DisplayName("DoD")
+                ServiceController1.Stop();
+
+
+
+    End Sub
